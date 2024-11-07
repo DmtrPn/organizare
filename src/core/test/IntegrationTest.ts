bd8116a65145a705d7e6b78db07ecfc92c7685f4
Dima Panov <rpsdpano@gmail.com>
2024-11-07 17:31:37 +0400

    fix tests

--- src/core/test/IntegrationTest.ts
+++ src/core/test/IntegrationTest.ts
@@ -1,38 +1,19 @@
 import { Inject } from 'typescript-ioc';
 
 import '@core/test/testRunner';
-import { BeforeAll } from '@core/test/decorators/testDecorators';
-import { createUser } from '@users/use-case/user.CreateCommand';
-import { UserData } from '@users/domain/user.types';
 import { IUserCrudService } from '@users/domain/IUserCrudService';
-import { isDefined } from '@utils/isDefined';
-
-interface CommonData {
-    user: UserData;
-}
+import { TestCommonData } from '@core/test/TestCommonData';
 
 export abstract class IntegrationTest {
     @Inject protected userCrudService!: IUserCrudService;
 
-    protected commonData: CommonData = {
-        user: {
-            id: '3ef2645f-2077-4562-b495-f8e58d10f3e5',
-            chatId: '3ef2645f-2077-4562-b495-f8e58d10f3e5',
-            firstName: 'firstName',
-            lastName: 'firstName',
-        },
-    };
-
-    @BeforeAll()
-    public async beforeAll(): Promise<void> {
-        const user = await this.userCrudService.getById(this.commonData.user.id);
-
-        if (!isDefined(user)) {
-            await createUser(this.commonData.user);
-        }
-    }
+    protected commonData = TestCommonData.getInstance();
 
     protected get chatId(): string {
-        return this.commonData.user.chatId;
+        return this.commonData.data.user.chatId;
+    }
+
+    protected get userId(): string {
+        return this.commonData.data.user.id;
     }
 }
--- src/core/test/testRunner.ts
+++ src/core/test/testRunner.ts
@@ -3,11 +3,14 @@ import { before, after } from 'node:test';
 
 import { DbConnector } from '@core/db-connector/DbConnector';
 import '@core/di/testIoC';
+import { TestCommonData } from '@core/test/TestCommonData';
 
 const dbConnector = DbConnector.getInstance();
 
 before(async () => {
     await dbConnector.initialize();
+    const testCommonData = TestCommonData.getInstance();
+    await testCommonData.init();
 });
 
 after(async () => {
